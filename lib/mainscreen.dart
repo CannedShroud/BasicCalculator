29a2be15a5b5e33f460eb2e1fd44a81e403384a5
CannedShroud <hypersync112@gmail.com>
2020-04-27 22:52:28 +0530

    Add results pane

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -3,18 +3,33 @@ import 'package:flutter/material.dart';
 import 'roundedSquareButton.dart';
 
 class MainScreen extends StatelessWidget {
+  String result = '0';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
-          Container(
-            color: kDark,
-            height: 260,
-            width: double.infinity,
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: kDark,
+              // height: 260,
+              width: double.infinity,
+              child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Text(
+                      result,
+                      style: TextStyle(color: Colors.white, fontSize: 100),
+                    ),
+                  )),
+            ),
           ),
           Expanded(
+            flex: 3,
             child: Container(
               color: Colors.grey[900],
               child: Column(
@@ -23,46 +38,91 @@ class MainScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      RoundedSquareButton(color: kBlue,title: 'π',),
-                      RoundedSquareButton(color: kBlue,title: 'x²',),
-                      RoundedSquareButton(color: kBlue,title: '√',),
-                      RoundedSquareButton(color: kBlue,title: '😦')
+                      RoundedSquareButton(
+                        color: kBlue,
+                        title: 'π',
+                      ),
+                      RoundedSquareButton(
+                        color: kBlue,
+                        title: 'x²',
+                      ),
+                      RoundedSquareButton(
+                        color: kBlue,
+                        title: '√',
+                      ),
+                      RoundedSquareButton(color: kBlue, title: '😦')
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      RoundedSquareButton(color: kDark,title: '7',),
-                      RoundedSquareButton(color: kDark,title: '8',),
-                      RoundedSquareButton(color: kDark,title: '9',),
-                      RoundedSquareButton(color: kBlue,title: '/')
-                    ],
-                  ),
-                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      RoundedSquareButton(color: kDark,title: '4',),
-                      RoundedSquareButton(color: kDark,title: '5',),
-                      RoundedSquareButton(color: kDark,title: '6',),
-                      RoundedSquareButton(color: kBlue,title: 'x')
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      RoundedSquareButton(color: kDark,title: '1',),
-                      RoundedSquareButton(color: kDark,title: '2',),
-                      RoundedSquareButton(color: kDark,title: '3',),
-                      RoundedSquareButton(color: kBlue,title: '+')
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '7',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '8',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '9',
+                      ),
+                      RoundedSquareButton(color: kBlue, title: '/')
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      RoundedSquareButton(color: kDark,title: '.',),
-                      RoundedSquareButton(color: kDark,title: '0',),
-                      RoundedSquareButton(color: kDark,title: 'C',),
-                      RoundedSquareButton(color: kBlue,title: '=')
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '4',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '5',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '6',
+                      ),
+                      RoundedSquareButton(color: kBlue, title: 'x')
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '1',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '2',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '3',
+                      ),
+                      RoundedSquareButton(color: kBlue, title: '+')
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '.',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: '0',
+                      ),
+                      RoundedSquareButton(
+                        color: kDark,
+                        title: 'C',
+                      ),
+                      RoundedSquareButton(color: kBlue, title: '=')
                     ],
                   ),
                 ],
